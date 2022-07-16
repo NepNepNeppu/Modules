364db4dcf6de21078e084eadafaa9fc850b99c25
NepNepNeppu <93623012+NepNepNeppu@users.noreply.github.com>
2022-07-16 04:16:04 -0400

    commit

--- Request/WaitFor.lua
+++ Request/WaitFor.lua
@@ -1,12 +1,60 @@
-return function (request)
-    local Format = request:split(".")
-    local World = game 
-    for _,data in pairs(Format) do
-        if World:FindFirstChild(data) ~= nil then
-            World = World[data]
+local World = {}
+
+    function World.isNested(request : string,preset : Instance?)
+        local Format = request:split(".")
+        local Class = preset or game
+        for _,data in pairs(Format) do
+            if Class:FindFirstChild(data) ~= nil then
+                Class = Class[data]
+            else
+                return tostring(Class.Name) == tostring(Format[#Format]),Class
+            end
+        end 
+        return true,Class
+    end
+
+    function World.getRandomID()
+		local randomGenerator = Random.new()
+		local largeNumber = randomGenerator:NextInteger(-os.time(), os.time())
+		return largeNumber   
+    end
+
+    function World.waitFor(request : string,preset : Instance?)
+        local Format = request:split(".")
+        local Class = preset or game
+        for _,data in pairs(Format) do
+            if Class:FindFirstChild(data) ~= nil then
+                Class = Class[data]
+            else
+                Class = Class:WaitForChild(data)
+            end
+        end
+        return Class
+    end
+
+    function World.applyWithoutMass(object : BasePart)
+        object.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0, 0, 0)
+        object.Massless = true
+    end
+
+    function World.lockedObject(item : string,name : string,Parent,update)
+        if Parent:FindFirstChild(name) == nil then
+            local Item = World.newObject(item,name,Parent)
+            return function ()
+                update(Item,false)
+            end
         else
-            World = World:WaitForChild(data)
+            return function ()
+                return Parent:FindFirstChild(name),true
+            end
         end
     end
-    return World
-end
+
+    function World.newObject(item : string,name : string,Parent)
+        local Item = Instance.new(item)
+        Item.Name = name
+        Item.Parent = Parent
+        return Item
+    end
+
+return World
